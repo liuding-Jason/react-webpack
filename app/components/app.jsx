8dd492ade4b04a08c4b16d033fd25f64256ffd51
liuding <liuding@ifmuse.com>
2017-03-29 14:36:33 +0800

    add redux

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,27 +1,37 @@
 
+// React and React-Router
 import { Router, Route, hashHistory } from 'react-router';
 import React , {Component} from 'react';
 import {render} from 'react-dom';
+
+// Redux
+import {createStore} from "redux" ;
+import counter from "../reducers" ;
 
 import Home        from "./ld-home" ;
 import List        from "./ld-list";
 
 import SetDpr from "../util/dpr" ;
 
+
+// let { subscribe, dispatch, getState } = createStore(reducer);
+new SetDpr().set() ;
+const store = createStore(counter) ;
+const rootEle = document.getElementById('myApp') ;
+
 function initRouter (){
-
-	new SetDpr().set() ;
-
 	render((
 	  <Router history={hashHistory}>
 	    <Route path="/" component={Home}/>
 	    <Route path="/home" component={Home}/>
 	    <Route path="/list" component={List} />
 	  </Router>
-	), document.getElementById('myApp') , function(){
+	), 
+	rootEle ,  
+	function(){
 	    console.log("项目初始化...");
 		/*实现项目的初始化*/
 	});
 }
-
-setTimeout(initRouter , 0);
+initRouter() ;
+store.subscribe(initRouter) ;
---
+++ app/reducers/index.js
@@ -0,0 +1,11 @@
+
+module.exports = (state = 0 , action) => {
+	switch(action.type){
+		case 'INCREASE' :
+			return state + 1 ;
+		case 'DECREASE' :
+			return state - 1 ;
+		default :
+			return state ;
+	}
+}
